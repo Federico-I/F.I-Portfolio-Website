d14e455ef5a7163937fbdb8c8828906e9968622c
Threxos <threxos.48@gmail.com>
2023-01-25 23:22:37 +0100

    contacts-done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div className=" flex flex-col items-center justify-center bg-stone-100 h-full w-full rounded-b-lg font-personalText text-3xl">
-      Contact
+    <div className=" flex flex-col items-center sm:flex-row sm:justify-between bg-stone-100 h-full w-full rounded-b-lg font-personalText text-3xl">
+      <h1 className="flex py-8 text-4xl sm:text-5xl sm:p-5">Contact</h1>
+      <div className=" flex flex-col items-center justify-items-center sm:flex-row ">
+        <h1 className="flex text-3xl p-3 sm:p-3 sm:selfcenter">Email:</h1>
+        <p className="flex font-planeText sm:p-3">fede.izzi33@gmail.com</p>
+      </div>
+      <div className="p-3">
+        <ul className=" flex flex-row sm:pr-8">
+          <li className="p-5">
+            <a href="https://github.com/Federico-I">
+              <FaGithub className="h-10 w-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
+            </a>
+          </li>
+          <li className="p-5">
+            <a href="https://linkedin.com/in/federico11izzi11">
+              <FaLinkedin className="h-10 w-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import About from "../components/About";
 function Home() {
   return (
     <div className=" flex flex-col items-center justify-center w-full h-full  pt-10 bg-transparent capitalize text-center">
-      <div className="py-8 w-full font-titleFont text-white rounded-t-lg text-3xl md:bg-black md:opacity-75">
-        <h2 className="py-3 ">Some of my work</h2>
-        <p className="text-left p-7 underline decoration-cyan-600">
+      <div className="py-8 w-full font-titleFont text-white rounded-t-lg  md:bg-black md:opacity-75">
+        <h2 className="py-3 text-4xl ">Some of my work</h2>
+        <p className="text-left text-3xl p-7 underline decoration-cyan-600">
           Most relevant projects
         </p>
       </div>
