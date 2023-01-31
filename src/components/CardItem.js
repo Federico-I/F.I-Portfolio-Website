681c7ddd0a3f69368d015cb3602486e4e2741b74
Federico-I <threxos.48@gmail.com>
2023-01-31 22:28:27 +0100

    background-color-carditem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CardItem() {
-  return <div className="px-3 py-7 bg-blue-500 rounded m-4">CardItem</div>;
+  return <div className="px-3 py-7 bg-red-500 rounded m-4">CardItem</div>;
 }
 
 export default CardItem;
