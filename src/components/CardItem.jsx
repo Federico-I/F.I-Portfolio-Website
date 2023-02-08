8ff3aa9d80c82b2036040851a96c67092e37ad6b
Federico-I <threxos.48@gmail.com>
2023-02-08 20:58:29 +0100

    carditem-boxing

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function CardItem() {
-  return <div className="px-3 py-7 bg-blue-500 rounded m-4">CardItem</div>;
+  return <div className="px-3 py-7 bg-green-500 m-4 rounded">
+    <div className="px-3 py-2 bg-blue-500 rounded m-4">CardItem</div>
+  </div>;
 }
 
 export default CardItem;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,7 +3,7 @@ import CardItem from "./CardItem";
 
 function ItemList() {
   return (
-    <div className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center w-full rounded-t-lg md:rounded-none bg-stone-100">
+    <div className="flex grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center w-full rounded-t-lg md:rounded-none bg-stone-100">
       <CardItem />
       <CardItem />
       <CardItem />
