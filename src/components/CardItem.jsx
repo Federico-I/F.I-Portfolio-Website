110d203d0138555106bd504c39a6f328fe221471
Federico-I <threxos.48@gmail.com>
2023-02-13 22:33:58 +0100

    card-link

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -4,7 +4,7 @@ function CardItem() {
   return <div className="px-7 py-8 bg-green-500 m-4 rounded">
     <div className="px-10 py-10 h-full bg-blue-500 rounded ">ImageComp</div>
     <div className="pt-4" >
-      <a href=""><p className="bg-yellow-500 rounded">git hub repo</p></a>
+      <a href="" target="_blank" rel="noreferrer"><p className="bg-yellow-500 rounded">git hub repo</p></a>
     </div>
   </div>;
 }
