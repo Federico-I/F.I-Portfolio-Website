3587efbd8ec8c1fb0e4ade7b4ae678bbaa38a22c
Federico-I <threxos.48@gmail.com>
2023-03-16 23:20:06 +0000

    card-img-item

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import GitHubProfilePhoto from "../Data/Images/Git-Hub-Profile-Item.jpg";
 
 function CardItem() {
+
+  const GitProfileItem = GitHubProfilePhoto;
+
   return <div className="px-7 py-8 bg-green-500 m-4 rounded">
-    <div className="px-10 py-10 h-full bg-blue-500 rounded ">ImageComp</div>
+    <div><a href="https://github.com/Federico-I" target="_blank" rel="noreferrer"><img src={GitProfileItem} alt="project" className="h-40 w-60 bg-blue-500 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-750"></img></a></div>
     <div className="pt-4" >
-      <a href="" target="_blank" rel="noreferrer"><p className="bg-yellow-500 rounded">git hub repo</p></a>
+      
+      <p className="bg-yellow-500 rounded">git hub repo</p>
     </div>
   </div>;
 }
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,8 +3,7 @@ import CardItem from "./CardItem";
 
 function ItemList() {
   return (
-    <div className="flex grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center w-full rounded-t-lg md:rounded-none bg-stone-100">
-      <CardItem />
+    <div className="flex grid grid-cols-1 md:grid-cols-2 place-items-center justify-space-around w-full rounded-t-lg md:rounded-none bg-stone-100">
       <CardItem />
       <CardItem />
     </div>
