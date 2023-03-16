22453fb27fe69a537d7a2fcf2dfefe1d745e4abd
Federico-I <threxos.48@gmail.com>
2023-03-16 23:58:51 +0000

    cardItem-itemList-newDesign-animation-style-done

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,7 +3,7 @@ import CardItem from "./CardItem";
 
 function ItemList() {
   return (
-    <div className="flex grid grid-cols-1 md:grid-cols-2 place-items-center justify-space-around w-full rounded-t-lg md:rounded-none bg-stone-100">
+    <div className="flex grid grid-cols-1 lg:grid-cols-2 place-items-center justify-space-around w-full rounded-t-lg md:rounded-none bg-stone-100">
       <CardItem />
       <CardItem />
     </div>
