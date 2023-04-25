0d5ff38855cf3bf6afb6404033905e9f5d02da74
Federico-I <threxos.48@gmail.com>
2023-04-25 21:35:52 +0100

    text-update

--- src/Data/infoData.js
+++ src/Data/infoData.js
@@ -6,7 +6,7 @@ export const informationData = [
     description:
       "This are some projects worked on during the learning process.",
     personal:
-      "Hey my name is Federico and I am a Front-End Web Developer. I have uruguayan and italian citizenship but I was born and raised in Uruguay, South America and I am 27 years old. When I was 21 years old I moved to Europe, where I currently live. Moreover, as a native Spanish speaker I have found very exiting to learn English.",
+      "Hey my name is Federico and I am a Front-End Web Developer. This web page was made from scratch. I am activly working on it adding more functionallity and improve the current features. As a self taught web developer I work full time and keep studying to improve my skills and learn new knowledge Moreover, as a native Spanish speaker I have found very exiting to learn English.",
     moreover:
       " Since the first time I started coding I have found my passion in it. Also, I keep practicing to perfectionate my skills as a developer. This field is very interesting to me because of the customazation and innovation it has. I belive, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million an one improvemnts to be made on this website but I will keep updating it with new fetures.",
   },
