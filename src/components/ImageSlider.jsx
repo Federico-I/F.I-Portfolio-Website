8f3146c17e9b2cffb2a1c3bae4f1a2f3500ca0d2
Federico-I <threxos.48@gmail.com>
2023-05-09 22:49:17 +0100

    imgLinks-sliderLogic-notes

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const photoSlider = [()];
 
+/* 
+
+slider logic - 
+
+need to create state funtion and store current position. 
+update state on every onClick (event-listener) to setState and change slides
+
+style buttons (animation?)
+
+*/
+
 const MainSlider (photoSlider) {
-    <div >
+    <div>
         <div>Slider</div>
     </div>
 };
