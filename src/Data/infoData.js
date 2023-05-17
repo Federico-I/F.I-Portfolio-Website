9e6ff1bcb5db3a7430dd19172a25b30ce0c22910
Federico-I <threxos.48@gmail.com>
2023-05-17 22:49:59 +0100

    slider-arrObjects-architecture-done

--- src/Data/infoData.js
+++ src/Data/infoData.js
@@ -10,6 +10,13 @@ export const informationData = [
     moreover:
       " Since the first time I started coding I have found my passion in it. This field is very interesting to me because of the customazation and innovation it has. I belive, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million and one improvemnts to be made on this website but I will keep updating it with new ideas, fetures and news.",
     ImgLinksSlide:
-    ["https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg", "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", "https://www.irishcentral.com/uploads/assets-v2/2020/12/resized_The_Rock_of_Cashel_master_via_Tourism_Ireland.jpg", "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", "https://velvetescape.com/wp-content/uploads/2010/09/engjell-gjepali-M0OIyN5u8ZM-unsplash.jpg"],  
+    [
+      {url:"https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", ulrName: ""},
+      {url: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg", urlName: ""},
+      {url: "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", urlName: ""},
+      {url: "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", urlName: ""},
+      {url: "https://velvetescape.com/wp-content/uploads/2010/09/engjell-gjepali-M0OIyN5u8ZM-unsplash.jpg", urlName: ""},
+      {url: "", urlName: ""},
+    ],
   },
 ];
