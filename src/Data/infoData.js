9b4f0510fc0de9c3fcd732d0893258fea185eb9f
Federico-I <threxos.48@gmail.com>
2023-05-21 19:43:25 +0100

    image-name-updated

--- src/Data/infoData.js
+++ src/Data/infoData.js
@@ -11,12 +11,11 @@ export const informationData = [
       " Since the first time I started coding I have found my passion in it. This field is very interesting to me because of the customazation and innovation it has. I belive, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million and one improvemnts to be made on this website but I will keep updating it with new ideas, fetures and news.",
     ImgLinksSlide:
     [
-      {url:"https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", ulrName: ""},
-      {url: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg", urlName: ""},
-      {url: "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", urlName: ""},
-      {url: "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", urlName: ""},
-      {url: "https://velvetescape.com/wp-content/uploads/2010/09/engjell-gjepali-M0OIyN5u8ZM-unsplash.jpg", urlName: ""},
-      {url: "", urlName: ""},
+      {url:"https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", ulrName: "China"},
+      {url: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg", urlName: "Austria"},
+      {url: "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", urlName: "U.S"},
+      {url: "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", urlName: "Scotland"},
+      {url: "https://velvetescape.com/wp-content/uploads/2010/09/engjell-gjepali-M0OIyN5u8ZM-unsplash.jpg", urlName: "Italy"},
     ],
   },
 ];
