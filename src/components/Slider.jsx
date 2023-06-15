92eafec27c40f8177423460416daff013ef9645b
Federico-I <threxos.48@gmail.com>
2023-06-15 21:55:54 +0100

    sliderSyles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,12 +5,24 @@ function Slider ({ imgs }) {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
+    const sliderContainer = {
+        height: "100%",
+        width: "relative",
+    };
+
     const imgsStyles = {
+        width: "100%",
+        height: "100%",
+        borderRadious: "10px",
+        backgroundposition: "center",
+        backgroundsize: "cover",
         backgroundImage: `url(${imgs[currentSlide]}.url)`,
     };
         
     return (
-        <div>
+        <div style={sliderContainer}>
+            <div styles={leftArrow}><</div>
+            <div styles={rightArrow}>></div>
             <div style={imgsStyles}></div>
         </div>
     );
