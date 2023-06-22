a31b3d24a024c71fddadf7d851dfdadbafa158c4
Federico-I <threxos.48@gmail.com>
2023-06-23 00:28:54 +0100

    moreover-text

--- src/Data/infoData.js
+++ src/Data/infoData.js
@@ -8,7 +8,7 @@ export const informationData = [
     personal:
       "Hey my name is Federico and I am a Front-End Web Developer. This web page was made from scratch. I am activly working on it, adding more functionallity and improving the current features. As a self taught web developer I work full time and keep studying to improvemy skills and learn new knowledge.Moreover, as a native Spanish speaker I have found very exiting to learn English.",
     moreover:
-      " Since the first time I started coding I have found my passion in it. This field is very interesting to me because of the customazation and innovation it has. I belive, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million and one improvemnts to be made on this website but I will keep updating it with new ideas, fetures and news.",
+      " Since the first time I started coding I have found my passion in it. This field is very interesting to me because of the customazation and innovation it has. I belive, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million and one improvemnts to be made on this website but I will keep updating it with new ideas and features.",
     ImgLinkSlides:
     [
       {url:"https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", ulrName: "China"},
