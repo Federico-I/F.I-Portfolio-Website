7505d8649b6431f661b0ae3f1ebfc50d57715bc7
Federico-I <threxos.48@gmail.com>
2023-06-29 22:54:29 +0100

    slider-backGround-bug-cantReadUrl

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -3,21 +3,24 @@ import Slider from "./Slider";
 
 function MainSlider({ imgData }) {
 
-    const sliderContainer = {
-        height: "100%",
-        width: "100%",
-        backgroundColor: "blue",
-    };
-
     const imgsPhotos = imgData.map( (item) => {
         return (item.imgLinkSlides);
     });
 
+    // const backgroundTest = imgsPhotos[0].url;
+    
+    const sliderContainer = {
+        height: "100%",
+        width: "100%",
+        /* backgroundImage: `url(${imgsPhotos[0].url})`, */
+    };
+
+    
 
     return (
-        <div className="p-10 flex flex-col" style={sliderContainer}>
+        <div className="flex flex-col p-10" style={sliderContainer}>
             <h1>Slider Test</h1>
-            <Slider imgs={imgsPhotos} />
+            <Slider  imgs={imgsPhotos} />
         </div>
     )
     
