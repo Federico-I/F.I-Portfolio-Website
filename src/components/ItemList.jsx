39dd55c42d68539ccdada03fe536f8e11f598df4
Federico-I <threxos.48@gmail.com>
2023-07-26 22:06:15 +0100

    listTitle-adjust-sliderImg-effect

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,8 +3,8 @@ import CardItem from "./CardItem";
 
 function ItemList() {
   return (
-    <div className="flex place-items-center grid grid-cols-1 w-full md:rounded-none bg-stone-100">
-      <h1 className="felx p-5 text-4xl md:text-5xl underline decoration-orange-600">
+    <div className="flex place-items-center grid grid-cols-1 w-full md:rounded-none bg-stone-100 rounded-t-lg">
+      <h1 className="felx p-5 text-5xl md:text-5xl underline decoration-green-500 font-titleFont">
         Projects
       </h1>
       <div className="flex grid grid-cols-1 lg:grid-cols-2 place-items-center justify-space-around w-full p-10 rounded-t-lg ">
