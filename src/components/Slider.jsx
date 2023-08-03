364ce460ee992199866e939072a56d8c861487b9
Federico-I <threxos.48@gmail.com>
2023-08-03 20:13:57 +0100

    slider-structure-change

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FontAwesomeIcon } from "react-icons/fa";
 
 function Slider({ imgs }) {
 
@@ -27,12 +27,12 @@ function Slider({ imgs }) {
             <div className="w-full font-titleFont text-white md:px-10 rounded-lg md:bg-black md:opacity-90 capitalize">
                 <div className="flex flex-col justify-center w-full h-full md:p-10 bg-transparent text-center">
                     <div className="flex flex-col justify-evenly ">
-                        <img className="p-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-750 opacity-75 hover:opacity-100 cursor-pointer" src={backgroundTest} alt="test"></img>
+                        <img className="p-8 transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-750 opacity-75 hover:opacity-100 cursor-pointer" src={backgroundTest} alt="test"></img>
                         <div className="flex flex-row justify-around p-5 ">
                             <FaArrowLeft className=" text-3xl md:text-5xl hover hover:text-red-500 hover:bg-white rounded-full cursor-pointer"/>
                             <span>
                                 {imgs.map((_, imgID) => {
-                                    <button key={imgID} onClick={null} className="indicator"></button>
+                                    <FontAwesomeIcon icon="fa-regular fa-circle-dot" key={imgID} onClick={null} className="indicator"/>
                                 })}
                             </span>
                             <FaArrowRight className="text-3xl md:text-5xl hover hover:text-green-400 hover:bg-white rounded-full cursor-pointer" />
