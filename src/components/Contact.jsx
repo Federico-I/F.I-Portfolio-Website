f1a845216e51d3f1ec943ae9eff1dcf9e6fc1028
Federico-I <threxos.48@gmail.com>
2023-09-12 11:31:44 +0100

    contact-mailto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,9 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 function Contact() {
+
+  const emailDieStrhal = "diestrahl7@gmail.com";
+
   return (
     <div className=" flex flex-col items-center xl:flex-row justify-between bg-stone-100 h-full w-full rounded-b-lg font-personalText text-3xl">
       <div className=" flex flex-col justify-items-center">
@@ -12,7 +15,7 @@ function Contact() {
       </div>
       <div className=" flex flex-col items-center justify-items-center lg:flex-row ">
           <h1 className="flex text-4xl p-3 sm:p-3 sm:selfcenter ">Email:</h1>
-          <p className="flex font-planeText sm:p-3 hover:underline hover:decoration-green-500 transition-all duration-1000 cursor-pointer">diestrahl7@gmail.com</p>
+          <a href={`mailto:${emailDieStrhal}`}><p className="flex font-planeText sm:p-3 hover:underline hover:decoration-green-500 transition-all duration-1000 cursor-pointer">{emailDieStrhal}</p></a>
       </div>
       <div className="p-3">
           <ul className=" flex flex-col lg:flex-row md:pr-8">
