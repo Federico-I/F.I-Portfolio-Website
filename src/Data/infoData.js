4dac41abdbc83a761b6bf07d27f3b0899ae728c1
Federico-I <threxos.48@gmail.com>
2023-09-26 22:41:50 +0100

    info-text-updated

--- src/Data/infoData.js
+++ src/Data/infoData.js
@@ -1,12 +1,17 @@
 export const informationData = [
   {
     id: 1,
+
     name: "Fede",
+
     title: "Selftought Junior Web Developer",
+
     description:
-      "This are some projects worked on during the learning process.",
+      "Don't know where to travel next? Let me give you some ideas!",
+
     personal:
-      "Hey my name is Federico and I am a Front-End Web Developer. This web page was made from scratch. I am activly working on it, adding more functionallity and improving the current features. As a self taught web developer I work full time and keep studying to improve my skills and continue learning.",
+      "Hey my name is Federico and I am a Front-End Web Developer. This web page was made from scratch. Activly working on it, improving the current features is the main task at the moment but I will add new fueatures of course. As a self taught web developer, I work full time and make time to keep studying to improve my skills.",
+
     moreover:
       " Since the first time I started coding I have found my passion in it. This field is very interesting to me because of the customazation and innovation it has. I believe, how important to read and write onece was for our ancestors, to code, will equaly be for the next generations. There's still a million and one improvments to be made on this website but I will keep updating it with new ideas.",
     
@@ -14,8 +19,8 @@ export const informationData = [
       [
         {url:"https://www.thisiscolossal.com/wp-content/uploads/2016/05/kyon03.jpg", label: "China -Zhangjiajie National Forest Park",  id:0},
         {url: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i2c81a69087b406ef/version/1456237268/most-beautiful-landscapes-in-europe-hallstatt-copyright-canadastock-european-best-destinations.jpg", label: "Austria", id:1},
-        {url: "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", label: "U.S - National Forest", id:2},
-        {url: "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", label: "Scotland", id:3},
+        {url: "https://www.nationalforests.org/assets/header-images/_1000x1000_fit_center-center_80_none/CopyMetolius.jpg", label: "U.S - National Parks", id:2},
+        {url: "https://cheaphotels4uk.com/wp-content/uploads/2017/08/highlands-scotland-bg.jpg", label: "The Quiraings, Isle of Skye, Scotland", id:3},
         {url: "https://velvetescape.com/wp-content/uploads/2010/09/engjell-gjepali-M0OIyN5u8ZM-unsplash.jpg", label: "Italy", id:4},
       ],
 
