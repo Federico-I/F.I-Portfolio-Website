e499e93b36679e05c01e95533c1bbc6c6e71bbf9
Federico-I <threxos.48@gmail.com>
2023-10-03 12:18:03 +0100

    card-info-blueprint

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -11,7 +11,26 @@ function CardItem({ defa }) {
 
   const getLabel = defaultInfo.label;
 
-  // map cardProyect to rener info
+  // map cardProyect to render info - still working on it.
+
+  /* 
+   
+  const cardInfo = defa.cardProject[0];
+
+  {cardInfo.map((infoItems)=> (
+    <div className="px-7 py-8 bg-slate-500 m-4 text-white hover:text-yellow-400 rounded">
+        <div>
+          <a href="infoItems.link" target="_blank" rel="noreferrer">
+            <img src={infoItems.imageProject} alt="project" className="h-40 w-70 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-750 opacity-75 hover:opacity-100 "></img>
+          </a>
+        </div>
+        <div className="pt-4 ">
+          <p className=" font-sans font-bold text-2xl rounded">{infoItems.projectLable}</p>
+        </div>
+      </div>
+  ))}
+
+  */
 
   return (
       <div className="px-7 py-8 bg-slate-500 m-4 text-white hover:text-yellow-400 rounded">
