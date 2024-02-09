35dc0391282c098bc62e9c6d095bea8fedafc8e6
Federico-I <threxos.48@gmail.com>
2024-02-09 18:14:35 +0000

    structring-ToDoList

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const List = [];
+
+function ToDoList() {
+
+    // useState
+
+    return(
+        <div>
+            <div>
+                <label>Input To Do</label>
+                <input></input>
+                <button></button>
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+};
