468e3f10c563d6547fcf81b0dfdbeb3f88090ef4
Federico-I <threxos.48@gmail.com>
2024-03-07 00:28:35 +0000

    tasky-add-styling-about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About({ itemData }) {
     <>
       <div className="flex flex-col items-center justify-center w-full h-full bg-stone-300 rounded-t-lg">
         <div className=" w-full leading-relaxed font-titleFont text-3xl">
-          <h1 className="flex justify-center xl:justify-start xl:pl-40 p-5 text-5xl md:text-5xl underline decoration-cyan-500 text-white bg-black font-titleFont rounded-t-lg">
+          <h1 className="flex justify-center xl:justify-start xl:pl-40 p-5 text-5xl md:text-5xl underline decoration-green-400 text-white bg-black font-titleFont rounded-t-lg">
             About
           </h1>
           <div className=" flex flex-col xl:flex-row pt-10 items-center justify-between">
