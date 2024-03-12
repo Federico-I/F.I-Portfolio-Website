31c0af071f937a086c8d84482bb1847c5a4ef6d0
Federico-I <threxos.48@gmail.com>
2024-03-12 23:31:12 +0000

    description-comp-style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Header(props) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-1/2 min-w-full text-8xl font-extrabold text-white py-80">
+    <div className="flex flex-col items-center justify-center w-full h-1/2 min-w-full text-6xl md:text-8xl font-extrabold text-white py-80">
       <p className="text-center">{props.text}</p>
     </div>
   );
