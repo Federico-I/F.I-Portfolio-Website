8c0ab5f40f2a66aa2380614ddc5b4618a54c67ac
Federico-I <threxos.48@gmail.com>
2024-04-16 22:38:54 +0100

    handleDelete-working

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -26,8 +26,8 @@ const list = [
 function NoTasks() {
 
     return( 
-        <li className="flex flex-row justify-between bg-blue h-16 bg-blue-500 hover:text-white rounded-xl m-4">
-            <p className="flex m-2 md:m-4 font-bold text-sm md:text-2xl">NO TASK YET</p>
+        <li className="flex flex-row justify-between bg-blue h-16 bg-blue-500 rounded-xl m-4">
+            <p className="flex m-2 md:m-4 font-bold text-sm md:text-2xl">ADD NEW TASK</p>
         </li>
     )
 };
@@ -98,14 +98,16 @@ function TaskList({ generalTasks, onHandleSelect, handleDeleteTask }) {
     //     fix code not ready yet
     /////////////////////////////////
 
+    const tasksLength = generalTasks.length;
+
     return(
         <div className="flex flex-col py-2 md:p-4 md:m-5">
             <label className="flex justify-center md:h-16 bg-yellow-400 p-2 m-3 md:m-5 text-gray-700 text-sm md:text-2xl font-bold">Next task to do is...
             </label>
-            <ul className="grid h-64 md:h-96 overflow-auto bg-black rounded-xl md:p-6">
-                {generalTasks.map((task) => 
-                    <TaskItem individualTask={task} key={task.id} onHandleSelect={onHandleSelect} handleDeleteTask={handleDeleteTask}/>    
-                )}
+            <ul className="flex flex-col h-64 md:h-96 overflow-auto bg-black rounded-xl md:p-6">
+                { tasksLength >= 1 ? generalTasks.map((task) => 
+                    <TaskItem individualTask={task} key={task.id} onHandleSelect={onHandleSelect} handleDeleteTask={handleDeleteTask}/>) : <NoTasks />
+                }
             </ul>
         </div>
     );
@@ -174,7 +176,7 @@ function InputTasks({ onAddTask, generalTasks, showContent, taskSelected, handle
                     <label className="bg-yellow-400 p-3 md:p-10 block text-gray-700 text-sm md:text-2xl font-bold mb-5">What do you need to do today?</label>
 
                     <div className=" flex flex-col justify-items-start mb-5">
-                        <label className=" w-20 p-1 bg-blue-400 text-white text-sm md:text-xl font-bold mb-2 rounded-xl">Task</label>
+                        <label className=" w-20 p-1 bg-blue-500 text-white text-sm md:text-xl font-bold mb-2 rounded-xl">Task</label>
                         <input className=" w-44 md:w-52 shadow appearance-none border rounded text-sm md:text-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)}/>
                     </div>
 
@@ -183,7 +185,7 @@ function InputTasks({ onAddTask, generalTasks, showContent, taskSelected, handle
                         <textarea className="block w-full p-3 text-sm md:text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)}/>
                     </div>
 
-                    <Button type="button" onClick={handleSubmit} className="items-center w-28 md:w-60 md:p-5  bg-blue-300 hover:bg-green-500 rounded-lg text-xl md:text-2xl font-bold text-white italic">ADD</Button>
+                    <Button type="button" onClick={handleSubmit} className="items-center w-28 md:w-60 md:p-5  bg-blue-500 hover:bg-green-500 rounded-lg text-xl md:text-2xl font-bold text-white italic">ADD</Button>
                 </form>
 
                 { showContent && <TaskDescription taskSelected={taskSelected} generalTasks={generalTasks}/>}
@@ -204,12 +206,12 @@ function TaskDescription({ generalTasks, taskSelected }) {
    const infoTask = generalTasks.map((task) => task.id === taskSelected.id);
 
     return(
-        <div className="grid w-48 md:w-80 justify-items-center bg-red-300 hover:bg-green-400 rounded-xl p-2 md:p-4 my-2 md:my-4">
+        <div className="grid w-48 md:w-80 justify-items-center bg-red-500 hover:bg-green-500 rounded-xl p-2 md:p-4 my-2 md:my-4">
 
             <h1 className="bg-yellow-400 p-2 block text-gray-700 text-sm md:text-2xl font-bold mb-2 md:mb-5">Task Description</h1>
             <div className="flex flex-col justify-items-start p-3 mb-1 md:mb-5 bg-blue-200 rounded-xl">
                 <h3 className="col-start-1 text-red-700 text-sm md:text-xl font-bold md:mb-2 underline"> Pick up present{infoTask.task}</h3>
-                <p className="p-2 md:p-4 mt-2 md:mt-4 bg-slate-300 rounded-xl text-balance text-sm md:text-md font-bold">Pick up Katty's present before 17hrs.{infoTask.details}</p>
+                <p className="p-2 md:p-4 mt-2 md:mt-4 bg-white rounded-xl text-balance text-sm md:text-md font-bold">Pick up Katty's present before 17hrs.{infoTask.details}</p>
             </div>
         </div>
     );
