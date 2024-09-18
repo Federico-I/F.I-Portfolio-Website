6676f58c2a77ac881965b1c523001615c787d1ff
Federico-I <threxos.48@gmail.com>
2024-09-18 23:53:16 +0100

    contact-responsiveStyling-ProfileIMG-styling-Fixed

--- src/components/Features/ProfileImage.jsx
+++ src/components/Features/ProfileImage.jsx
@@ -9,7 +9,7 @@ function ProfileImage() {
       <img
         src={profilePicture}
         alt="profilePhoto"
-        className="flex rounded-full h-40 w-40 sm:h-50 sm:w-50 lg:h-50 lg:w-50 xl:h-80 xl:w-80 transition-all duration-1000 cursor-pointer filter grayscale hover:grayscale-0 drop-shadow-md hover:drop-shadow-2xl transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-750"
+        className="flex rounded-full h-40 w-40 sm:h-60 sm:w-60 lg:h-80 lg:w-80 transition-all duration-1000 cursor-pointer filter grayscale hover:grayscale-0 drop-shadow-md hover:drop-shadow-2xl transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-1000"
       />
       <h1 className="flex m-10 text-4xl md:text-6xl">Federico Izzi</h1>
     </div>
